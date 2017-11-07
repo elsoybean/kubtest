e901e6a62bc9fb685341475957841e296cc4505d
Chris Lee <cmlee1014@gmail.com>
2017-11-07 00:13:27 -0500

    Add RabbitMQ service

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -5,6 +5,9 @@ kubectl create -f query/service.yaml
 kubectl create -f command/deployment.yaml
 kubectl create -f command/service.yaml
 
+kubectl create -f messagebus/deployment.yaml
+kubectl create -f messagebus/service.yaml
+
 kubectl create configmap nginx-frontend-conf --from-file=frontend/nginx.conf
 kubectl create -f frontend/deployment.yaml
 kubectl create -f frontend/service.yaml
--- kubernetes/stop.sh
+++ kubernetes/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl delete service query command frontend
-kubectl delete deployment query command frontend
+kubectl delete service query command messagebus frontend
+kubectl delete deployment query command messagebus frontend
 kubectl delete configmap nginx-frontend-conf
