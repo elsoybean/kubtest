f119f73f702c1e0142a5d0fd8c5440dd53934ea3
Chris Lee <cmlee1014@gmail.com>
2017-11-15 13:06:40 -0500

    Update projection to connect to MySQL and execute queries

--- images/projection/server.js
+++ images/projection/server.js
@@ -1,41 +1,54 @@
-var _ = require('underscore'),
+const _ = require('underscore'),
     fs = require('fs'),
     amqp = require('amqplib/callback_api'),
-    config = JSON.parse(fs.readFileSync('./config.json', 'utf-8')),
+    mysql = require('mysql'),
+    configPath = './config.json',
     envPath = "./config." + process.env.NODE_ENVIRONMENT + ".json",
-    secretPath = "./secret/config.json";
+    secretPath = "./secrets/config.json";
 
-if (fs.existsSync(envPath)) {
-    var secretJson = JSON.parse(fs.readFileSync(envPath, 'utf-8'));
-    config = _.extend(config, secretJson);
-}    
+var config = JSON.parse(fs.readFileSync(configPath));
+if (fs.existsSync(envPath)) config = _.extend(config, JSON.parse(fs.readFileSync(envPath)));
+if (fs.existsSync(secretPath)) config = _.extend(config, JSON.parse(fs.readFileSync(secretPath)));
 
-if (fs.existsSync(secretPath)) {
-    var secretJson = JSON.parse(fs.readFileSync(secretPath, 'utf-8'));
-    config = _.extend(config, secretJson);
-}
+const db = mysql.createConnection(config.MySQL.ConnectionString);
+db.connect(function(err) {
+    if (err) {
+        console.error('error connecting to database: ' + err.stack);
+        return;
+    } else {
+        console.log('connected to database');
+    }
 
-amqp.connect(config.RabbitMQ.ConnectionString, function(err, conn) {
-    conn.createChannel(function(err, ch) {
-        var ex = config.RabbitMQ.Exchange;
-        ch.assertExchange(ex, 'topic', {durable: false});    
-        
-        ch.assertQueue('ColorTracker', {durable: true}, function(err, q) {
-            ch.bindQueue(q.queue, ex, 'Foo.FooCreated.*');
-            ch.bindQueue(q.queue, ex, 'Foo.ColorChanged.*');
-            ch.consume(q.queue, function(msg) {
-            	var e = JSON.parse(msg.content.toString(), 'utf-8');
-            	switch (e.EventType) {
-            		case "Foo.FooCreated":
-		            	var sql = "INSERT INTO ColorTracker (FooId, Color) VALUES ('" + e.ModelId + "', '" + e.Event.Color + "')";
-		                console.log(sql);
-            			break;
-            		case "Foo.ColorChanged":
-		            	var sql = "UPDATE ColorTracker SET Color = '" + e.Event.To + "' WHERE FooId = '" + e.ModelId + "'";
-		                console.log(sql);
-            			break;
-            	}
-            }, {noAck: true});
+    amqp.connect(config.RabbitMQ.ConnectionString, function(err, conn) {
+        if (err) {
+            console.error('error connecting to message bus: ' + err.stack);
+            return;
+        } else {
+            console.log('connected to message bus');
+        }
+
+        conn.createChannel(function(err, ch) {
+            var ex = config.RabbitMQ.Exchange;
+            ch.assertExchange(ex, 'topic', {durable: false});    
+            
+            ch.assertQueue('ColorTracker', {durable: true}, function(err, q) {
+                ch.bindQueue(q.queue, ex, 'Foo.FooCreated.*');
+                ch.bindQueue(q.queue, ex, 'Foo.ColorChanged.*');
+                ch.consume(q.queue, function(msg) {
+                    var e = JSON.parse(msg.content.toString(), 'utf-8');
+                    switch (e.EventType) {
+                        case "Foo.FooCreated":
+                            db.query("INSERT INTO ColorTracker (FooId, Color) VALUES (?,?)", [ e.ModelId, e.Event.Color ]);
+                            break;
+                        case "Foo.ColorChanged":
+                            db.query("UPDATE ColorTracker SET Color = ? WHERE FooId = ?", [ e.Event.To, e.ModelId ]);
+                            break;
+                        default:
+                            console.error("unknown event type: " + e.EventType);
+                            break;
+                    }
+                }, {noAck: true});
+            });
         });
     });
 });
